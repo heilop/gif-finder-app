621aa56b10e91badff8c4283ae3cdd7d78d3b8cb
Heissen Lopez <heissen.lopez@gmail.com>
2021-07-12 12:49:49 -0500

    Add classes and styles

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -27,18 +27,20 @@ export const GifGrid = ( { category }) => {
   }
 
   return (
-    <div>
+    <>
       <h3>{ category }</h3>
+      <div className="card-grid">
 
-      {
-        images.map(img => (
-          <GifGridItem
-            key = { img.id }
-            img = {img}
-            { ...img }
-          />
-        ))
-      }
-    </div>
+        {
+          images.map(img => (
+            <GifGridItem
+              key = { img.id }
+              img = {img}
+              { ...img }
+            />
+          ))
+        }
+      </div>
+    </>
   )
 }
