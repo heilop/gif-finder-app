0d23c0ab605ec210d3a7b4427cbb318400c1089e
Heissen Lopez <heissen.lopez@gmail.com>
2021-07-27 00:29:34 -0500

    Initial test for AddCategory

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,11 +4,18 @@ import { AddCategory } from '../../components/AddCategory';
 describe('Test in <AddCategory />', () => {
 
   const setCategories = () => {};
+  const wrapper = shallow(<AddCategory setCategories = { setCategories }/>);
+
   test('should show the right component', () => {
-
-    const wrapper = shallow(<AddCategory setCategories = { setCategories }/>);
-
     expect(wrapper).toMatchSnapshot();
+  })
+
+  test('should change the text field', () => {
+    const input = wrapper.find('input');
+    const value = 'Hello world!';
+    input.simulate('change', { target: { value } });
+
+    expect(wrapper.find('p').text().trim()).toBe(value);
   })
 
 })
