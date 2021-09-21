46f463636820ad54f6a5eb5529dd5e3337abbc56
Heissen Lopez <heissen.lopez@gmail.com>
2021-09-20 21:56:32 -0500

    Add custom categories for testing

--- src/tests/GifFinderApp.test.js
+++ src/tests/GifFinderApp.test.js
@@ -5,10 +5,17 @@ import GifFinderApp from '../GifFinderApp';
 
 
 describe('Test in <GifFinderApp />', () => {
-  let wrapper = shallow(<GifFinderApp />);
 
   test('should show <GifFinderApp /> correctly', () => {
+    const wrapper = shallow( < GifFinderApp / > );
     expect(wrapper).toMatchSnapshot();
+  })
+
+  test('should show a category list', () => {
+    const categories = ['Mr Robot', 'The Wire'];
+
+    const wrapper = shallow( < GifFinderApp defaultCategories = { categories}/ > );
+    expect(wrapper.find('GifGrid').length).toBe( categories.length );
   })
 
 })
