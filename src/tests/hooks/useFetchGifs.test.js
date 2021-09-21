f8df8bc2aea7c874fd8117ff3b46db3ac603a23f
Heissen Lopez <heissen.lopez@gmail.com>
2021-09-20 22:37:48 -0500

    Add tests for useFetchGifs hook

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -1,11 +1,24 @@
+import { renderHook } from "@testing-library/react-hooks";
 import { useFetchGifs } from "../../hooks/useFetchGifs";
 
 
 describe('Test in the useFetchGifs hook', () => {
-  test('should return the initial state', () => {
-    const { data, loading } = useFetchGifs(['Mr Robot']);
+  test('should return the initial state', async() => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('Mr Robot'));
+    const { data, loading } = result.current;
+    await waitForNextUpdate();
 
-    console.log(data. loading);
+    expect(data).toEqual([]);
+    expect(loading).toBe(true);
   })
 
+  test('should return one array of images and loading in false', async() => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('Mr Robot'));
+    await waitForNextUpdate();
+
+    const { data, loading } = result.current;
+
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
+  })
 })
